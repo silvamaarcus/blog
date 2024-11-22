535a0cf738696d4244b0827166ee26cfa24b255d
Marcus Sillion <marcus.costa@sillion.com.br>
2024-11-22 14:34:12 -0300

    chore: estrutura do projeto

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <main className="border bg-gray-950 text-white">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni tempore
+          facilis odio soluta eum ex at error et, ea omnis sint hic assumenda
+          sapiente praesentium, in cupiditate nesciunt non. Temporibus.
+        </p>
+      </main>
+      <h1 className="bg-gradient-1 bg-clip-text text-9xl text-transparent">
+        Teste.tsx
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "576px", // Substituindo o breakpoint padrão "sm"
+        md: "720px", // Substituindo o breakpoint padrão "md"
+        lg: "960px", // Substituindo o breakpoint padrão "lg"
+        xl: "1200px", // Substituindo o breakpoint padrão "xl"
+        // "2xl": "1920px", // Adicionando um novo breakpoint
+      },
+      backgroundImage: {
+        "gradient-1": "linear-gradient(225deg, #5390E3 8.12%, #1357B3 92.21%)",
+        "gradient-2": "linear-gradient(225deg, #1B69D2 0%, #07346F 100%)",
+      },
+      colors: {
+        customBlue: {
+          light: "#5390E3",
+          DEFAULT: "#1B69D2",
+          dark: "#1357B3",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
