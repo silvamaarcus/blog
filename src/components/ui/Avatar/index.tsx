c5341b42862abc161dd790ca3cf0618c609db175
Marcus Sillion <marcus.costa@sillion.com.br>
2024-11-25 11:24:03 -0300

    feat: componente Avatar

---
+++ src/components/ui/Avatar/index.tsx
@@ -0,0 +1,46 @@
+export interface AvatarProps {
+  imageUrl: string;
+  alt?: string;
+  className?: string;
+  size: "sm" | "md" | "lg";
+}
+
+const Avatar = ({ imageUrl, alt, className, size }: AvatarProps) => {
+  if (size === "sm") {
+    return (
+      <div>
+        <img
+          src={imageUrl}
+          alt={alt}
+          className={`h-[48px] w-[48px] rounded-full border-2 border-customBlue-light ${className}`}
+        />
+      </div>
+    );
+  }
+
+  if (size === "md") {
+    return (
+      <div>
+        <img
+          src={imageUrl}
+          alt={alt}
+          className={`h-[64px] w-[64px] rounded-full border-2 border-customBlue-light ${className}`}
+        />
+      </div>
+    );
+  }
+
+  if (size === "lg") {
+    return (
+      <div>
+        <img
+          src={imageUrl}
+          alt={alt}
+          className={`h-[72px] w-[72px] rounded-full border-2 border-customBlue-light ${className}`}
+        />
+      </div>
+    );
+  }
+};
+
+export default Avatar;
