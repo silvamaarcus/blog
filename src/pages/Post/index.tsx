88c46cf0a6e782155f6d6421175c4ed88216091e
Marcus Sillion <marcus.costa@sillion.com.br>
2024-11-27 16:47:57 -0300

    fix: pagina Post (requisicao)

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -2,44 +2,24 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import SocialMedias from "@/components/SocialMedias";
 import Avatar from "@/components/ui/Avatar";
+import getPosts from "@/hooks/getPosts";
 
-import api from "@/services/api";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
-interface Post {
-  id: number;
-  title: string;
-  content: string;
-  date: string;
-  category: string;
-  id_user: number;
-}
 
 const PostPage = () => {
-  const [post, setPost] = useState<Post | null>(null);
-  const [user, setUser] = useState([]);
+  const [post, setPost] = useState<any[]>([]);
 
-  const { idPost } = useParams();
-
-  // Fazendo requisição uma única vez por carregamento da página.
   useEffect(() => {
-    // if (idPost) -> verificar se existe algo dentro da variavel "idPost".
-    if (idPost) {
-      // 1° requisição: Pegar o Post escolhido pelo usuario.
-      api.get("/posts" + idPost).then((response) => {
-        // Setando o post escolhido na variavel "setPost".
-        setPost(response.data);
-
-        // 2° requisição: Pegar os dados Autor do Post escolhido pelo usuario.
-        // id_user -> é uma chave no arquivo JSON, que será extraido da requisição acima.
-        api.get("/user" + response.data.id_user).then((r) => {
-          // Setando o autor do post escolhido na variavel "setUser".
-          setUser(r.data);
-        });
-      });
-    }
-  });
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts({ star: 6, limit: 1, order: "desc" });
+        setPost(data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   return (
     <>
@@ -48,15 +28,15 @@ const PostPage = () => {
         <div className="mt-16 grid grid-cols-1 gap-8 text-center sm:grid-cols-12">
           <div className="invisible-grid col-span-2"></div>
 
-          {post && (
-            <div className="col-span-12 mb-16 sm:col-span-8">
+          {post.map((post, index) => (
+            <div className="col-span-12 mb-16 sm:col-span-8" key={index}>
               <div>
                 <p className="uppercase text-customGray-gray_5">{post.date}</p>
                 <p className="uppercase text-customBlue-light">
                   {post.category}
                 </p>
               </div>
-              <h3 className="text-h3 sm:text-h3Sm">titulo</h3>
+              <h3 className="text-h3 sm:text-h3Sm">{post.title}</h3>
 
               <div className="my-16 flex flex-col items-center justify-center gap-4 sm:flex-row">
                 <div className="flex flex-col items-center gap-4 sm:flex-row">
@@ -68,44 +48,20 @@ const PostPage = () => {
                 </div>
 
                 <p className="text-customGray-gray_5">
-                  Aug 2, 2020 - 8 min read
+                  {`${post.date.slice(3, 6)} ${post.date.slice(0, 2)}, ${post.date.slice(7, 11)} - ${post.duration} min read`}
                 </p>
               </div>
 
               <div>
                 <img
-                  src="https://assets-prd.ignimgs.com/2024/09/10/ps5-pro-full-blog-1725981773141.jpg"
-                  alt="PS5"
+                  src={post.imageUrl}
+                  alt={post.title}
                   className="max-h-[420px] w-full rounded object-cover"
                 />
 
                 <div className="mb-14 mt-10 text-left">
-                  <h4 className="text-h4 sm:text-h4Sm">
-                    Esse aqui é o primeiro título
-                  </h4>
-                  <p className="mt-2 text-customGray-gray_5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Ornare urna pharetra ut ac, pellentesque. Ultricies
-                    habitasse pretium purus viverra. Sit eget volutpat semper
-                    vitae metus, fringilla ullamcorper sapien nibh.
-                  </p>
-                </div>
-                <div className="mb-14 mt-10 text-left">
-                  <h4 className="text-h4 sm:text-h4Sm">
-                    Esse aqui é o segundo título
-                  </h4>
-                  <p className="mt-2 text-customGray-gray_5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Ornare urna pharetra ut ac, pellentesque. Ultricies
-                    habitasse pretium purus viverra. Sit eget volutpat semper
-                    vitae metus, fringilla ullamcorper sapien nibh.
-                  </p>
-                  <p className="mt-2 text-customGray-gray_5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Ornare urna pharetra ut ac, pellentesque. Ultricies
-                    habitasse pretium purus viverra. Sit eget volutpat semper
-                    vitae metus, fringilla ullamcorper sapien nibh.
-                  </p>
+                  <h4 className="text-h4 sm:text-h4Sm">{post.resume}</h4>
+                  <p className="mt-4 text-customGray-gray_5">{post.content}</p>
                 </div>
 
                 <div className="flex flex-wrap gap-8 rounded border border-customBlack-light p-8 sm:flex-nowrap">
@@ -144,7 +100,7 @@ const PostPage = () => {
                 </div>
               </div>
             </div>
-          )}
+          ))}
 
           <div className="invisible-grid col-span-2"></div>
         </div>
